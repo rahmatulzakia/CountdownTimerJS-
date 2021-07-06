3b15717c15e7ee55f89e4d71a3787d1f0fb971d6
rahmatulzakia15 <rahmatulzakia@gmail.com>
2021-07-06 11:17:20 +0700

    Countdown Timer With JavaScript

---
+++ script.js
@@ -0,0 +1,24 @@
+const timeH = document.querySelector('h1');
+let timeSecond = 5;
+
+displayTime(timeSecond)
+//timeH.innerHTML = `00:${timeSecond}`;
+
+const countDown = setInterval(() => {
+    timeSecond--;
+    displayTime(timeSecond);
+    if(timeSecond <=0 || timeSecond < 1){
+        endTime();
+        clearInterval(countDown);
+    }
+},1000)
+
+function displayTime(second){
+    const min = Math.floor(second / 60);
+    const sec = Math.floor(second % 60);
+    timeH.innerHTML = `${min<10 ?  '0' : ''}${min}:${sec<10 ? '0':''}${sec}`
+}
+
+function endTime(){
+    timeH.innerHTML = 'TIME OUT';
+}
